17153760beedbc02ca9db4501d07bfa0dfb978e7
HaydenLiu <huangxunliu@gmail.com>
2019-11-11 15:50:24 +1100

    Initial Commit

---
+++ wekol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wekol
+//
+//  Created by 刘皇逊 on 11/11/19.
+//  Copyright © 2019 Eidetika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
